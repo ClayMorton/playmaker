de68800d68bbdf17dc2d224b0ccabbb935cb308b
ClayMorton <mmorton1163@gmail.com>
2024-12-07 20:48:49 -0500

    Initializing the project

---
+++ Play Maker/Play Maker/Play_MakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Play_MakerApp.swift
+//  Play Maker
+//
+//  Created by Clay Morton on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Play_MakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Play Maker/Play MakerTests/Play_MakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Play_MakerTests.swift
+//  Play MakerTests
+//
+//  Created by Clay Morton on 12/7/24.
+//
+
+import Testing
+@testable import Play_Maker
+
+struct Play_MakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
